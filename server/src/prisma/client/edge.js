f35005f90b3d122160d0a2ef212a7dfa18957687
Grimm <rafalmirkowski@gmail.com>
2024-03-11 15:22:09 +0100

    add initial boilerplate

--- server/src/prisma/client/edge.js
+++ server/src/prisma/client/edge.js
@@ -143,8 +143,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
-    "schemaEnvPath": "../../../.env"
+    "rootEnvPath": null
   },
   "relativePath": "../../../prisma",
   "clientVersion": "5.8.1",
