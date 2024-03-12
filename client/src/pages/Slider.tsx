848b5c2b6ea967c2bc27bac8cf84832068c5eecf
Grimm <rafalmirkowski@gmail.com>
2024-03-12 11:54:57 +0100

    fix layout

--- client/src/pages/Slider.tsx
+++ client/src/pages/Slider.tsx
@@ -1,4 +1,4 @@
-import { Box, BoxFlexCol } from '../components/ui/Boxes';
+import { Box, BoxFlexCol, BoxFlexRow } from '../components/ui/Boxes';
 import React from 'react';
 import { Text } from '../components/ui/Text';
 import Button from '../components/ui/Button';
@@ -10,16 +10,17 @@ const Slider = () => {
           'w-[640px] h-[480px] mx-auto mt-32 rounded-xl border border-gray shadow-gray'
         }
       >
-        <Box className={'px-8 py-6'}>
+        <BoxFlexCol className={'px-8 py-6 h-full gap-2'}>
           <Text as={'h1'} variant={'bold-18'}>
             Collaborate by submitting slider value
           </Text>
-          <BoxFlexCol>
-            <Button className={'w-[320px] max-w-[320px] self-center'}>
+          <BoxFlexCol className={'h-full'}>
+            <BoxFlexRow className={'grow border border-gray'}>hello</BoxFlexRow>
+            <Button className={'w-[320px] max-w-[320px] my-8 self-center'}>
               Submit me
             </Button>
           </BoxFlexCol>
-        </Box>
+        </BoxFlexCol>
       </Box>
     </Box>
   );
