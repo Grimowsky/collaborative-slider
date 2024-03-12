bff75c02bde00b0085a52e9717ef334483e427a9
Grimm <rafalmirkowski@gmail.com>
2024-03-12 13:55:35 +0100

    extend slider component

---
+++ client/src/components/ui/Slider.tsx
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import * as SliderPrimitive from '@radix-ui/react-slider';
+import { cn } from '../../utils/className.utils';
+
+type SliderRoot = typeof SliderPrimitive.Root;
+const Root = React.forwardRef<
+  React.ElementRef<SliderRoot>,
+  React.ComponentPropsWithoutRef<SliderRoot>
+>((props, forwardedRef) => {
+  return (
+    <SliderPrimitive.Root
+      {...props}
+      ref={forwardedRef}
+      className={cn(
+        'relative flex items-center select-none w-full min-w-24 h-5',
+        props.className,
+      )}
+    />
+  );
+});
+Root.displayName = 'SliderRoot';
+
+type SliderTrack = typeof SliderPrimitive.Track;
+const Track = React.forwardRef<
+  React.ElementRef<SliderTrack>,
+  React.ComponentPropsWithoutRef<SliderTrack>
+>((props, forwardedRef) => {
+  return (
+    <SliderPrimitive.Track
+      {...props}
+      ref={forwardedRef}
+      className={cn('absolute w-full h-1 bg-gray', props.className)}
+    />
+  );
+});
+
+type SliderRange = typeof SliderPrimitive.Range;
+const Range = React.forwardRef<
+  React.ElementRef<SliderRange>,
+  React.ComponentPropsWithoutRef<SliderRange>
+>((props, forwardedRef) => {
+  return (
+    <SliderPrimitive.Range
+      {...props}
+      ref={forwardedRef}
+      className={cn('absolute h-full bg-black rounded-full', props.className)}
+    />
+  );
+});
+
+type SliderThumb = typeof SliderPrimitive.Thumb;
+const Thumb = React.forwardRef<
+  React.ElementRef<SliderThumb>,
+  React.ComponentPropsWithoutRef<SliderThumb>
+>((props, forwardedRef) => {
+  return (
+    <SliderPrimitive.Thumb
+      {...props}
+      ref={forwardedRef}
+      className={cn(
+        'block w-5 h-5 bg-primary shadow-md rounded-full hover:bg-secondary focus:outline-none focus:ring-2 focus:ring-accent',
+        props.className,
+      )}
+    />
+  );
+});
+
+export { Root, Track, Range, Thumb };
--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { routes } from './routes.schema';
 import { Route, Routes } from 'react-router-dom';
 import Login from '../pages/Login';
-import Slider from '../pages/Slider';
+import SliderForm from '../pages/SliderForm';
 
 export const AppRoutes = (): React.ReactElement => {
   return (
     <Routes>
       <Route path={routes.BASE_ROUTE} element={<Login />} />
-      <Route path={routes.SLIDER} element={<Slider />} />
+      <Route path={routes.SLIDER} element={<SliderForm />} />
     </Routes>
   );
 };
