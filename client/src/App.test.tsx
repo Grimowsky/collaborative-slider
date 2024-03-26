6e1f30bb465cbeb0aef577b7fd289ecde8a5ee84
Grimm <rafalmirkowski@gmail.com>
2024-03-26 10:56:22 +0100

    remove basic unit test

--- client/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { describe, it } from 'vitest';
-
-//TODO: remove this test  after forking repo
-describe('App basic test', () => {
-  it('something', () => {
-    expect(2).toBe(2);
-  });
-});
